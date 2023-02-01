bf472c030b5044f5bc656abfe2ff35d5c6f67ac8
Omarghalayini <42498579+Omarghalayini@users.noreply.github.com>
2023-02-01 00:25:36 +0000

    Add files via upload

---
+++ practic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){ // maths function
+
+
+    const double sales = 9500;
+    cout << "salary:£ " << sales << endl;
+
+    const double National_Insurance_Rate = .04;
+    double National_Insurance = sales * National_Insurance_Rate;
+    cout << "National_Insurance:£ "<< National_Insurance << endl;
+
+    const double county_tax_rate = .02;
+    double county_tax = sales * county_tax_rate;
+    cout <<"county_tax:$ " << county_tax << endl;
+
+    return 0;       
+}
